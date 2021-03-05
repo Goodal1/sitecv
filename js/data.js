d967affafe7702e6d6b2feba2ff2848e52d3e829
Goodal1 <58956923+Goodal1@users.noreply.github.com>
2021-03-05 22:35:38 +0100

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,53 @@
+const xp=[
+
+{
+	date:'2018-2020',
+	titre:'Jose John',
+	boite:'Maxiprout',
+	tags:'tag1,tag2,tag3,taglong,tagpluslong,tag5,tag6',
+	lien:'maxijose.com'
+
+},
+{
+	date:'2018-2020',
+	titre:'Jose John',
+	boite:'Maxiprout',
+	tags:'tag1,tag2,tag3,taglong,tagpluslong,tag5,tag6',
+	lien:'maxijose.com'
+
+},
+{
+	date:'2018-2020',
+	titre:'Jose John',
+	boite:'Maxiprout',
+	tags:'tag1,tag2,tag3,taglong,tagpluslong,tag5,tag6',
+	lien:'maxijose.com'
+
+},
+{
+	date:'2018-2020',
+	titre:'Jose John',
+	boite:'Maxiprout',
+	tags:'tag1,tag2,tag3,taglong,tagpluslong,tag5,tag6',
+	lien:'maxijose.com'
+
+},
+{
+	date:'2018-2020',
+	titre:'Jose John',
+	boite:'Maxiprout',
+	tags:'tag1,tag2,tag3,taglong,tagpluslong,tag5,tag6',
+	lien:'maxijose.com'
+
+},
+{
+	date:'2018-2020',
+	titre:'Jose John',
+	boite:'Maxiprout',
+	tags:'tag1,tag2,tag3,taglong,tagpluslong,tag5,tag6',
+	lien:'maxijose.com'
+
+},
+
+
+]
