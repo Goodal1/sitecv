fbcad1c4e4adcb0a57a460b4c1cb0f8b5c22f65e
Goodal1 <58956923+Goodal1@users.noreply.github.com>
2021-03-06 00:37:40 +0100

    Experience

---
+++ js/data.js
@@ -0,0 +1,53 @@
+const xp=[
+
+{
+	date:'2018-2020',
+	titre:'Jose John 1',
+	boite:'N\'Py Nouvelles Pyrénées',
+	tags:'tag1,tag2,tag3,taglong,tagpluslong,tag5,tag6',
+	lien:'maxijose.com'
+
+},
+{
+	date:'2018-2020',
+	titre:'Jose John 2',
+	boite:'N\'Py Nouvelles Pyrénées',
+	tags:'tag1,tag2,tag3,taglong,tagpluslong,tag5,tag6',
+	lien:'maxijose.com'
+
+},
+{
+	date:'2018-2020',
+	titre:'Jose John 3',
+	boite:'N\'Py Nouvelles Pyrénées',
+	tags:'tag1,tag2,tag3,taglong,tagpluslong,tag5,tag6',
+	lien:'maxijose.com'
+
+},
+{
+	date:'2018-2020',
+	titre:'Jose John 4',
+	boite:'N\'Py Nouvelles Pyrénées',
+	tags:'tag1,tag2,tag3,taglong,tagpluslong,tag5,tag6',
+	lien:'maxijose.com'
+
+},
+{
+	date:'2018-2020',
+	titre:'Jose John 5',
+	boite:'N\'Py Nouvelles Pyrénées',
+	tags:'tag1,tag2,tag3,taglong,tagpluslong,tag5,tag6',
+	lien:'maxijose.com'
+
+},
+{
+	date:'2018-2020',
+	titre:'Jose John 6',
+	boite:'N\'Py Nouvelles Pyrénées',
+	tags:'tag1,tag2,tag3,taglong,tagpluslong,tag5,tag6',
+	lien:'maxijose.com'
+
+},
+
+
+]
--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,3 +1,5 @@
+
+//MENU MOBILE
 function deployMenu(){
     const burger = document.querySelector('.burger');
     const mobileMenu = document.querySelector('.menu-mobile');
@@ -12,22 +14,74 @@ function deployMenu(){
     })
 }
 
-deployMenu();
 
-const main = document.querySelector('.main-banner');
-main.addEventListener('mouseover', function(e){
+//MOUVEMENT DU ROBOT
+function moveRobot(){
+    const main = document.querySelector('.main-banner');
+    const robot = document.querySelector('#robot');
+
+    main.addEventListener('mouseover', function(e){
     let walk = e.x/10;
-    document.querySelector('#robot').style.transform=`translateX(${walk}px) translateY(20%) rotate(10deg)`
-})
+    robot.style.transform=`translateX(${walk}px) translateY(20%) rotate(10deg)`
+    })
 
-main.addEventListener('mouseout', function(e){
-    document.querySelector('#robot').style.transform=`translateX(0px) translateY(25%) rotate(0deg)`
-})
+    main.addEventListener('mouseout', function(e){
+    robot.style.transform=`translateX(0px) translateY(25%) rotate(0deg)`
+    })
+}
 
-const darkmode = document.querySelector('#darkmode');
-darkmode.addEventListener('click',function(){
-    document.documentElement.style.setProperty('--dark1', '#fff');
-    document.documentElement.style.setProperty('--dark2', '#F4F7F6')
-    document.documentElement.style.setProperty('--white', '#000');
 
-})
+//DARKMODE
+function darkMode(){
+    const darkmode = document.querySelector('#darkmode');
+    let dark=true;
+    
+    darkmode.addEventListener('click',function(){
+        const root = document.documentElement;
+        dark = !dark;
+ 
+
+        if (dark){
+            root.style.setProperty('--dark1', '#26283B');
+            root.style.setProperty('--dark2', '#0E0000')
+            root.style.setProperty('--white', '#fff');
+        } else{
+            root.style.setProperty('--dark1', '#fff');
+            root.style.setProperty('--dark2', '#F4F7F6')
+            root.style.setProperty('--white', '#000');
+        }
+        
+        })
+}
+
+//XPSLIDER
+
+const sliderContainer = document.querySelector('.xp-slider-container');
+
+for (var i = 0; i < xp.length; i++) {
+
+    const date = `<p class="xp-date">${xp[i].date}</p>`;
+    const titre = `<h3>${xp[i].titre}</h3>`;
+    const boite = `<p class="xp-boite">${xp[i].boite}</p>`;
+    const lien = `<p>${xp[i].lien}</p>`;
+    const tagsList = Array.from(xp[i].tags.split(','));
+    const tags = tagsList.map(tag => `<span>${tag}</span>`).join('');
+
+    const textnode = document.createElement('div');
+    textnode.innerHTML=`${date}${titre}${boite}${tags}${lien}`;
+    sliderContainer.appendChild(textnode);
+
+    
+
+    
+
+
+    }
+
+
+
+
+
+/*deployMenu();
+moveRobot();
+darkMode();*/
